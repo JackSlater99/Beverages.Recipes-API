b6b82a5b349984e0b8e350c4a1ccda2ab15b187e
Jack Slater <101419891+JackSlater99@users.noreply.github.com>
2022-08-22 20:52:07 +0100

    Delete back-end-beverages-recipes directory

--- back-end-beverages-recipes/src/main/java/com/example/backendbeveragesrecipes/repositories/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backendbeveragesrecipes.repositories;
-
-import com.example.backendbeveragesrecipes.models.RawIngredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<RawIngredient, Long> {
-}
--- back-end-beverages-recipes/src/main/java/com/example/backendbeveragesrecipes/repositories/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendbeveragesrecipes.repositories;
-
-
-import com.example.backendbeveragesrecipes.models.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-
-}
