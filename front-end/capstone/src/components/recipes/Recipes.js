3c1f12fff1a594a9890556d9035fe71d8cec8fed
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-08-25 11:52:51 +0100

    api fetched for home

--- front-end/capstone/src/components/recipes/Recipes.js
+++ front-end/capstone/src/components/recipes/Recipes.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RecipeCard from "./RecipeCard";
 
 const Recipes = () => {
+  const apiUrl = "http://localhost:8080/api/recipes";
+
+  const [recipesJson, setRecipesJson] = useState({});
+
+  const getAllRecipes = async () => {
+    const resp = await fetch(apiUrl);
+    const data = await resp.json();
+    setRecipesJson(data);
+  };
+
+  useEffect(() => {
+    getAllRecipes();
+  }, []);
+
   return (
     <>
       <h2>Recipes Index (**Recipes**)</h2>
