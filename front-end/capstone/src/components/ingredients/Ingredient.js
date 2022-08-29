19bdb1a64acc8c96b09faabc3f45de7bbb74e98e
Mike GM1WKR <gm1wkr@gmail.com>
2022-08-29 21:42:34 +0100

    added recipe card

--- front-end/capstone/src/components/ingredients/Ingredient.js
+++ front-end/capstone/src/components/ingredients/Ingredient.js
@@ -1,46 +1,42 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import RecipeCard from '../recipes/RecipeCard';
 
 const Ingredient = ({ ingredients, recipes }) => {
 
-    const {id} = useParams();
+  const { id } = useParams();
 
-    const foundIngredient = ingredients.find((currentIngredient) => { 
-            // console.log(currentIngredient.id);
-            return currentIngredient.id === parseInt(id);
-        }); 
-    // we need to compare every rawIngredient id to the recpies. 
+  const foundIngredient = ingredients.find((currentIngredient) => {
+    // console.log(currentIngredient.id);
+    return currentIngredient.id === parseInt(id);
+  });
+  // we need to compare every rawIngredient id to the recpies. 
 
 
-    const recipesWithIngredient = [];
+  const recipesWithIngredient = [];
 
-    for (let recipe of recipes){
-        for (let ingredient of recipe.ingredients){
-            if(ingredient.id === foundIngredient.id){
-                recipesWithIngredient.push(recipe);
-            }
-        }
+  for (let recipe of recipes) {
+    for (let ingredient of recipe.ingredients) {
+      if (ingredient.id === foundIngredient.id) {
+        recipesWithIngredient.push(recipe);
+      }
     }
+  }
 
-    // const recipesWithIngredient = recipes.map( (recipe) => {
-    //     return recipe.ingredients.filter((ingredient) => {
-    //         console.log(foundIngredient) ;
-    //         return  ingredient.rawIngredient.id === foundIngredient.id;
-
-    //     });
-    // });
+  const cardNodes = recipesWithIngredient.map((recipe) => {
+    return <RecipeCard recipe={recipe} key={recipe.id} />;
+  });
 
 
+  console.log(recipesWithIngredient);
 
-    console.log(recipesWithIngredient);
-    
-    return(
-        <>
-            <p>{foundIngredient.name}</p> 
-            <p>{foundIngredient.recipe}</p>
-        </>    
-            
-    )
+  return (
+    <>
+      <h2>Recipes using {foundIngredient.name}</h2>
+      <main className="grid grid-cols-4 gap-4">{cardNodes}</main>
+    </>
+
+  )
 }
 
 export default Ingredient;
