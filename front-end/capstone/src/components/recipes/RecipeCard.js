f08446f828a931625b0013b54c0538d430433278
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-08-31 15:20:56 +0100

    image size increased

--- front-end/capstone/src/components/recipes/RecipeCard.js
+++ front-end/capstone/src/components/recipes/RecipeCard.js
@@ -9,7 +9,7 @@ const RecipeCard = ({ recipe }) => {
   return (
     <article className="grid place-items-center pb-6">
       <h4>{recipe.name}</h4>
-      <img className="w-80 mb-4 rounded" src={recipe.image} alt={recipe.name} />
+      <img className="w-96 mb-4 rounded" src={recipe.image} alt={recipe.name} />
       <Link to={{ pathname: `/recipes/${recipe.id}` }} recipe={recipe}>
         Full Recipe
       </Link>
