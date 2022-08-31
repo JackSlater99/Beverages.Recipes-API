a2c576e06b5727987f7a3648aa75e372d9de5ff7
user <user@users-MacBook-Pro.local>
2022-08-31 15:26:50 +0100

    slicing

--- front-end/capstone/src/components/recipes/Recipes.js
+++ front-end/capstone/src/components/recipes/Recipes.js
@@ -10,17 +10,19 @@ const Recipes = ({ recipes }) => {
     <>
       <h2>Recipes Index (**Recipes**)</h2>
       <h2 id="cocktail">Cocktail</h2>
-      <main className="grid grid-cols-4 gap-4">{cardNodes[0]}</main>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(0, 5)}</main>
       <h2 id="coffee">Coffee</h2>
-      <main className="grid grid-cols-4 gap-4">{cardNodes.slice[0, 5]}</main>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(5, 9)}</main>
+      <h2 id="fruit">Fruit Juice</h2>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(9, 14)}</main>
       <h2 id="martini">Martini</h2>
-      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(6, 10)}</main>
-      <h2 id="mocktail">Mocktail</h2>
-      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(6)}</main>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(14, 16)}</main>
       <h2 id="tea">Tea</h2>
-      <main className="grid grid-cols-4 gap-4">{cardNodes.slice()}</main>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(16, 17)}</main>
       <h2 id="milk">Milk</h2>
-      <main className="grid grid-cols-4 gap-4">{cardNodes.slice()}</main>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(17, 18)}</main>
+      <h2 id="milkshake">Milkshake</h2>
+      <main className="grid grid-cols-4 gap-4">{cardNodes.slice(18, 20)}</main>
     </>
   );
 };
